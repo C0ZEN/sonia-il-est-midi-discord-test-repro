95a47501279b4ebae8cd15dabd9a98e8826609c6
C0ZEN <geoffrey.testelin@gmail.com>
2020-03-25 09:51:30 +0100

    1 file modified, 2 added, 1 deleted

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { getWelcomeMessage } from "./get-welcome-message";
-
-const username = "Marco Polo";
-const welcomeMessage: string = getWelcomeMessage(username);
-
-console.log(welcomeMessage);
